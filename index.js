be648a1a46d15c8cca8fb6b3d9ac8fd1a81dc091
Eugenio Miró <eugenio.miro@gmail.com>
2019-02-19 16:11:57 -0300

    add initial application

---
+++ index.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+var server = http.createServer(function(request, response) {
+    response.writeHead(200, {'Content-Type': "text/plain"});
+    response.end("Hello Euge!");
+});
+
+var port = process.env.PORT || 3030;
+server.listen(port);
+
+console.log("Server running at port %d", port);
