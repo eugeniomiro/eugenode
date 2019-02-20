e29a29c636f3e10e1e1bb20f2f9219da845bd9dd
Eugenio Miró <eugenio.miro@gmail.com>
2019-02-20 08:20:25 -0300

    add login view

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const path          = require('path');
 const cookieParser  = require('cookie-parser');
 const indexRouter   = require('./routes/index');
 const usersRouter   = require('./routes/users');
+const loginRouter   = require('./routes/login');
 
 const app           = express();
 
@@ -19,6 +20,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/',      indexRouter);
 app.use('/users', usersRouter);
+app.use('/login', loginRouter);
 
 app.use(function(req, res, next) {
     next(createErrors(404));
