3a1cffc0354868a0386abfb84be626157bb1e9b0
Eugenio Miró <eugenio.miro@gmail.com>
2019-02-24 21:56:26 -0300

    fix login/logout

--- routes/login.js
+++ routes/login.js
@@ -4,7 +4,6 @@ var router    = express.Router();
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.locals.errors = req.flash('error');
-  console.log(res.locals.errors);
   res.render('login', { title: 'Euge Node' });
 });
 
