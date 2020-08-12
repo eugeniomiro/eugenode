3ca1a020242653fbc1d4d08c6d458b9095c7a5ca
Eugenio Miró <eugenio.miro@gmail.com>
2020-08-12 00:37:00 -0300

    fix mongodb test

--- tests/mongodb.spec.js
+++ tests/mongodb.spec.js
@@ -2,7 +2,7 @@ describe("MongoDB", function() {
     it("is there a server running", function(next) {
         var MongoClient = require('mongodb').MongoClient;
         MongoClient.connect('mongodb://127.0.0.1:27017/eugenode', 
-                            { useNewUrlParser: true }, 
+                            { useNewUrlParser: true, useUnifiedTopology: true }, 
                             function(err, db) {
             expect(err).toBe(null);
             expect(db).toBeDefined();
